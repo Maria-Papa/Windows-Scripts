1060923813be8899b4301c7c90fea60a64554bfe
Maria-Papa <1maria.papa@gmail.com>
2021-11-16 19:54:40 +0200

    Bat scripts to auto run VM, Chrome + Code-Server & Alacritty

---
+++ Development Startup.bat
@@ -0,0 +1,6 @@
+"D:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm --type headless "Development New"
+timeout /t 60 /nobreak
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory="Profile 1"
+"C:\Program Files\Alacritty\alacritty.exe"
+exit
+
---
+++ WebDev Startup.bat
@@ -0,0 +1,5 @@
+"D:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm --type headless "WebDev"
+timeout /t 60 /nobreak
+"C:\Program Files (x86)\Google\Chrome\Application\chrome_proxy.exe"  --profile-directory="Profile 1" --app-id=jbcbikjbmpcpnbefbpheldgicfpbbadn
+"C:\Program Files\Alacritty\alacritty.exe"
+exit
